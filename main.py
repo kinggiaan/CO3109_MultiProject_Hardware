51b577825a077501f82be2f9faa3aaea4b20526c
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-03-31 09:09:01 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#TODO: 1. herculous dua data len | CHECKED
+#TODO: 2. get weather data in python
+
+import random
+# import pyowm  # import Python Open Weather Map to our project.
+# from pyowm import OWM
+import serial.tools.list_ports
+import time
+import sys
+import requests
+# from Adafruit_IO import MQTTClient
+
+AIO_FEED_ID = ["multi-projct.button", "multi-projct.sound"]
+AIO_USERNAME = "kinggiaan"
+AIO_KEY = "aio_oyFJ6970b4VKFcRDqBkU3Y7gkTse"
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+            print(commPort)
+    return commPort
+
+isMicrobitConnected = False
+if getPort() != "None":
+    ser = serial.Serial( port=getPort(), baudrate=115200)
+    isMicrobitConnected = True
+
+
+
+# ser = serial.Serial(port=getPort(), baudrate=115200)
+
+mess = ""
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[0] == "BUTTON":
+        r = requests.get('https://thay-tam.herokuapp.com/api/v1/ping')
+        print('Gui request thanh cong')
+        print(r)
+        print(r.json())
+    # if splitData[0] == "HUMI":
+        # client.publish("multi-projct.sound", splitData[1])
+
+
+mess = ""
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+
+count30s=30
+while True:
+    if isMicrobitConnected:
+        readSerial()
+
+    time.sleep(1)
